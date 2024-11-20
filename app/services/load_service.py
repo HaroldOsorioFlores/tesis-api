b8c3fb84cf62a4b4cfe27379a005d1a0a1a03dc5
HaroldOsorioFlores <haroldosorio32@gmail.com>
2024-11-20 15:31:52 -0500

    se anadio el modelo a s3

--- app/services/load_service.py
+++ app/services/load_service.py
@@ -2,6 +2,8 @@ import joblib
 import os
 import pandas as pd
 import numpy as np
+import boto3
+
 
 # servicio de carga del modelo de machine learning
 class ModelService:
@@ -9,25 +11,28 @@ class ModelService:
         self.knn_loaded = None
         self.nombres_productos = None
         self.combinaciones_productos = None
+        self.s3 = boto3.client('s3')
 
     async def load_model_and_data(self):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
+        await self.download_file('mi-bucket-de-modelos', 'modelo_knn.joblib', '/tmp/modelo_knn.joblib')
+        await self.download_file('mi-bucket-de-modelos', 'combinaciones_productos.csv', '/tmp/combinaciones_productos.csv')
 
-        file_path_model = os.path.join(current_dir, '../data/modelo_knn.joblib')
         print("Iniciando carga del modelo...")
-        self.knn_loaded = joblib.load(file_path_model)
+        self.knn_loaded = joblib.load('/tmp/modelo_knn.joblib')
         print(f"Modelo cargado: {self.knn_loaded}")
 
-
-        # Cargar el archivo CSV
-        file_path_csv = os.path.join(current_dir, '../data/combinaciones_productos.csv')
         print("Iniciando carga del archivo CSV...")
-        df_cp = pd.read_csv(file_path_csv, sep=';')
+        df_cp = pd.read_csv('/tmp/combinaciones_productos.csv', sep=';')
         self.combinaciones_productos = np.array(df_cp)
         print(f"Archivo CSV cargado: {self.combinaciones_productos}")
 
         print("Modelo y datos cargados correctamente")
 
+    async def download_file(self, bucket_name, object_key, file_path):
+        if not os.path.exists(file_path):
+            print(f"Descargando {object_key} desde S3...")
+            self.s3.download_file(bucket_name, object_key, file_path)
+            print(f"Archivo {object_key} descargado correctamente")
    
 
 model_service = ModelService()
